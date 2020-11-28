44927962c114af681853b32e76108a0d0b508f5a
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-11-28 16:23:14 +0800

    Added background-image to menu-item component

--- src/components/menu-item.jsx
+++ src/components/menu-item.jsx
@@ -3,10 +3,15 @@ import './menu-item.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
     <div className={`${size} menu-item`}
-         style={{backgroundImage:`url(${imageUrl})`}}    
+           
     >
+        <div className='background-image'
+             style={{backgroundImage:`url(${imageUrl})`}}  
+        >
+
+        </div>
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>BUY NOW</span>
         </div>
     </div>
