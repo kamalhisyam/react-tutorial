5dc6e88c907536f08691f29a10d8e34f2bb534b2
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-11-28 15:38:49 +0800

    Added custom size as menu-item property

--- src/components/menu-item.jsx
+++ src/components/menu-item.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './menu-item.scss';
 
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}
+         style={{backgroundImage:`url(${imageUrl})`}}    
+    >
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>BUY NOW</span>
