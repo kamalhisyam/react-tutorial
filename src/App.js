4cef2e702a8dcf7be611e16c0ca399c7547cccbb
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-11-28 23:49:35 +0800

    Added Header and logo. Clicking on Logo brings to home page.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/shop/shop';
 import { Route, Switch} from 'react-router-dom';
+import Header from './components/header/header';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route path='/shop' component={ShopPage}/>
         <Route path ='/' component={HomePage}/>
