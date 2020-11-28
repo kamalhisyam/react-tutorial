ca6432dfc62041f021d8b3f220d21aa4e61835e7
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-11-28 12:26:55 +0800

    Added HomePage.jsx component and styling

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import  "./HomePage.scss";
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>ITEM</h1>
+                    <span>BUY NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
