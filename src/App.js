435d2b22a61796925781d67a004dec2d2796f22c
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-11-28 18:25:35 +0800

    Implemented routing demo for Hats page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import HatsDemo from './pages/HatsDemoPage';
+import { Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-       <HomePage/>
+      <Switch>
+        <Route exact path='/shop/hats' component={HatsDemo}/>
+        <Route path ='/' component={HomePage}/>
+        
+      </Switch>
+       
     </div>
   );
 }
---
+++ src/pages/HatsDemoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HatsDemo = () => (
+    <div>
+        <h1>HATS DEMO PAGE</h1>
+    </div>
+);
+
+export default HatsDemo;
