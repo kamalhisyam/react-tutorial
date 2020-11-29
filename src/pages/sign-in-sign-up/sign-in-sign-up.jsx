c7ed66e9305dafc16ce081c389677b6bc1030e49
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-11-29 18:15:08 +0800

    Added sign in page

--- src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SignIn from '../../components/sign-in/sign-in';
 import './sign-in-sign-up.scss';
 
 const SignInSignUpPage = () => (
     <div className='sign-in-and-sign-up-page'>
-        SIGN IN
+        <SignIn/>
     </div>
 );
 
