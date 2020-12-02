4d7b76be2501bc4f57559f6c49a8e324cdba5784
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-12-02 09:29:33 +0800

    Created additional selectors. Implemented selectors using createStructuredSelectors

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/shop/shop';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up'
-import { Redirect, Route, Switch} from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Header from './components/header/header';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 import './App.css';
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    
-    const {setCurrentUser} = this.props;
+
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
@@ -23,15 +25,15 @@ class App extends React.Component {
         (await userRef).onSnapshot(snapshot => {
           setCurrentUser({
             currentUser: {
-            id: snapshot.id,
-            ...snapshot.data()
+              id: snapshot.id,
+              ...snapshot.data()
             }
           });
         })
       } else {
         setCurrentUser(userAuth);
       }
-    });    
+    });
   }
 
   componentWillUnmount() {
@@ -42,20 +44,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Switch>
-          <Route exact path ='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' render={() => this.props.currentUser? <Redirect to='/' /> : <SignInSignUpPage/>}/>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/' /> : <SignInSignUpPage />} />
         </Switch>
-        
+
       </div>
-  );
-}
+    );
+  }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -7,6 +7,7 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from 'react-redux';
 import CartActions from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from "reselect";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon">
@@ -19,6 +20,6 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(CartActions.toggleCartHidden())
 });
 
-const mapStateToProps = state => ({itemCount: selectCartItemsCount(state)});
+const mapStateToProps = createStructuredSelector({itemCount: selectCartItemsCount});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon) ;
