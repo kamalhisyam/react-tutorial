a7839e68013936820cae624342738177cbc94ef2
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-12-06 14:48:15 +0800

    Converted Shop data to an object for optimization.
    Created new selector for Collections Overview

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import CollectionItem from '../../components/collection-item/collection-item';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-    console.log(collection);        
+    const { title, items } = collection;      
     return (
-    <div className="category">
-        <h2>COLLECTION PAGE</h2>
+    <div className="collection-page">
+        <h2 className='title'>{ title.toUpperCase()}</h2>
+        <div className="items">
+            { items.map(item => (<CollectionItem key={item.id} item={item}/>))   }
+        </div>
     </div>
 );
     };
